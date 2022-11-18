8f672e7b03c3455055b8cb107e9dfdceaf79dbba
LDJ <ldj8200@naver.com>
2022-11-18 17:49:47 +0900

    2022.11.18 LDJ
    1. 권한체크 관련 코드 재작성
    2. 앱 크래쉬 제어용 임시 프래그먼트 생성
    3. 어플 시작과 동시에 지도 중앙화면 이동 기능 구현
    4. 플로팅 버튼 이벤트 구현
    5. 마커 구현용 임시 코드 생성(추후 기능완성시 삭제)

--- app/src/main/java/com/example/ev_check/MainActivity.kt
+++ app/src/main/java/com/example/ev_check/MainActivity.kt
@@ -2,14 +2,18 @@ package com.example.ev_check
 
 import android.Manifest
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationManager
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Looper
+import android.provider.Settings
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.FrameLayout
 import android.widget.TextView
@@ -17,8 +21,10 @@ import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.app.FrameMetricsAggregator
 import com.example.ev_check.fragment.MapFragment
+import com.example.ev_check.fragment.MypageFragment
 import com.google.android.gms.location.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.snackbar.Snackbar
 
 
 class MainActivity : AppCompatActivity() {
@@ -28,13 +34,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // -------------------------------------------------------------
+        /* ----------- 테스트 영역 (사용 후 삭제) ----------- */
 
-        // 변수 선언
+
+
+        /* ----------- 변수 선언 및 초기화 영역 ----------- */
+
         val bnv = findViewById<BottomNavigationView>(R.id.bnv)
         val fl = findViewById<FrameLayout>(R.id.fl)
 
-        // -------------------------------------------------------------
+        /* ----------- 이벤트 영역 ----------- */
 
         // 처음 실행시 bnv에 MapFragment가 보이도록 설정
         supportFragmentManager.beginTransaction().replace(
@@ -42,9 +51,7 @@ class MainActivity : AppCompatActivity() {
             MapFragment()
         ).commit() // 커밋 필수!!!
 
-        // -------------------------------------------------------------
-
-        // bnv를 클릭했을 때
+        // bnv(네비게이션 뷰) 클릭 이벤트
         bnv.setOnItemSelectedListener { item ->
             // item --> 내가 선택한 메뉴 정보
             when (item.itemId){
@@ -59,15 +66,29 @@ class MainActivity : AppCompatActivity() {
                     ).commit() // 커밋 필수!!!
                 }
 
+
+                R.id.tab4 -> {
+                    // tab4 선택시
+                    Toast.makeText(this, "네번째 부분화면", Toast.LENGTH_SHORT).show()
+                    // FrameLayout에 MypageFragment 부여
+                    supportFragmentManager.beginTransaction().replace(
+                        R.id.fl, // fragment를 넣을 곳
+                        MypageFragment() // 넣을 fragment
+                    ).commit() // 커밋 필수!!!
+                }
+
             }
             // false : event 처리가 끝나지 않았다고 판단
             // true : event 종료를 감지해서 다른 버튼 클릭 가능
             true
         }
-        // -------------------------------------------------------------
+
+        /* ----------- 기타 영역 ----------- */
 
 
     }
+
+    /* ----------- 함수, 메서드, 기타기능 영역 ----------- */
 
 
 }
--- app/src/main/java/com/example/ev_check/auth/IntroActivity.kt
+++ app/src/main/java/com/example/ev_check/auth/IntroActivity.kt
@@ -1,15 +1,18 @@
 package com.example.ev_check.auth
 
 import android.Manifest
-import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
+import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.app.ActivityCompat
+import com.example.ev_check.BuildConfig
 import com.example.ev_check.MainActivity
 import com.example.ev_check.R
 import com.google.android.gms.auth.api.signin.GoogleSignIn
@@ -17,6 +20,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.SignInButton
 import com.google.android.gms.common.api.ApiException
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
@@ -25,63 +29,78 @@ import com.google.firebase.ktx.Firebase
 
 class IntroActivity : AppCompatActivity() {
 
-    // ------------ 구글 로그인, 권한 설정에 필요한 변수 설정 ------------
+    /* ----------- 전역변수 선언 영역 ----------- */
 
     private val RC_SIGN_IN = 9001
     private var googleSignInClient : GoogleSignInClient?=null
     lateinit var auth : FirebaseAuth // 객체 공유 인스턴스
 
-    private val REQUEST_PERMISSION_LOCATION = 10 // 권한 체크용 변수
-    
-    // *** 스낵바를 위한 View 객체 firstLayout 생성
+    // private val REQUEST_PERMISSION_LOCATION = 10 // 권한 체크용 변수
 
-    // ----------------------------------------------------
+    // 스낵바 사용을 위한 레이아웃 뷰 객체 선언
+    lateinit var introPageLayout: View
+
+    // 필요 권한 모음 배열
+    var permission_list = arrayOf(
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION
+    )
+
+    val PERMISSIONS_REQUEST = 0x0000001
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
 
-        // ------------------------------------------------
+        /* ----------- 테스트 영역 (사용 후 삭제) ----------- */
+
+        OnCheckPermission()
 
 
+        /* ----------- 변수 선언 및 초기화 영역 ----------- */
+        introPageLayout = findViewById<ConstraintLayout>(R.id.introPageLayout)
 
-        // ------------------------------------------------
-
-        checkPermissionForLocation(this)
-
-        // ------------------------------------------------
-
+        // 파이어베이스
         auth = Firebase.auth
 
-        // ------------------------------------------------
-
+        // 구글연동로그인옵션
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken("209508508120-eu6to9irjj3trfubotcjbc393cc5p0cs.apps.googleusercontent.com")
             .requestEmail()
             .build()
 
-        // ------------------------------------------------
+        // 구글 signIn 버튼 객체
+        val signInGoogleBtn: SignInButton = findViewById(R.id.signInButton)
 
-        val signInGoogleBtn: SignInButton = findViewById(R.id.signInButton) // 구글 signIn 버튼 객체
 
-        signInGoogleBtn.setOnClickListener { // 구글로그인버튼 온클릭이벤트
-            //로그인 처리
-            googleSignInClient = GoogleSignIn.getClient(this, gso)
-            val signInIntent = googleSignInClient!!.signInIntent
-            startActivityForResult(signInIntent, RC_SIGN_IN)
+        /* ----------- 이벤트 영역 ----------- */
+
+        // 구글 로그인 버튼 온클릭이벤트
+        signInGoogleBtn.setOnClickListener {
+            // 로그인 처리
+            try{
+                googleSignInClient = GoogleSignIn.getClient(this, gso)
+                val signInIntent = googleSignInClient!!.signInIntent
+                startActivityForResult(signInIntent, RC_SIGN_IN)
+            }catch (e: ApiException){
+
+            }
+
         }
 
     } // create
 
-    // ----------------------------------------------------
+    /* ----------- 기타 영역 ----------- */
 
     override fun onDestroy() {
         super.onDestroy()
         firebaseAuthSignOut()
     }
 
-    // ----------------------------------------------------
+    /* ----------- 함수, 메서드, 기타기능 영역 ----------- */
 
+    // 구글로그인
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
@@ -100,8 +119,7 @@ class IntroActivity : AppCompatActivity() {
         }
     }
 
-    // ----------------------------------------------------
-
+    // 구글로그인시 받아오는 정보
     private fun firebaseAuthWithGoogle(idToken: String) {
         val credential = GoogleAuthProvider.getCredential(idToken, null)
         auth.signInWithCredential(credential)
@@ -129,40 +147,133 @@ class IntroActivity : AppCompatActivity() {
 
     }
 
-    // ----------------------------------------------------
-
+    // 파이어베이스 로그아웃
     private fun firebaseAuthSignOut() {
         Firebase.auth.signOut() // 로그아웃
         googleSignInClient!!.signOut() // 로그인할때마다 계정선택
     }
 
-    // 위치 권한이 있는지 확인하는 메서드
-    private fun checkPermissionForLocation(context: Context): Boolean {
-        // Android 6.0 Marshmallow 이상에서는 위치 권한에 추가 런타임 권한이 필요
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (context.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                true
+    // 권한 설정 체크
+    private fun OnCheckPermission() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    permission_list,
+                    PERMISSIONS_REQUEST
+                )
             } else {
-                // 권한이 없으므로 권한 요청 알림 보내기
-                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_PERMISSION_LOCATION)
-                false
+                ActivityCompat.requestPermissions(
+                    this,
+                    permission_list,
+                    PERMISSIONS_REQUEST
+                )
             }
-        } else {
-            true
         }
     }
 
-    // 사용자에게 권한 요청 후 결과에 대한 처리 로직
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+    // 권한 요청 결과 처리
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == REQUEST_PERMISSION_LOCATION) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-
+        when (requestCode) {
+            PERMISSIONS_REQUEST -> if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) { // 권한이 허용되었을 경우
+                Snackbar.make(introPageLayout, "권한이 설정되었습니다", Snackbar.LENGTH_LONG).show()
             } else {
-                Log.d("ttt", "onRequestPermissionsResult() _ 권한 허용 거부")
-                Toast.makeText(this, "권한이 없어 해당 기능을 실행할 수 없습니다.", Toast.LENGTH_SHORT).show()
+
+                Snackbar.make(
+                    introPageLayout,
+                    "앱 사용을 위해서는 권한 설정이 필요합니다.\n설정으로 이동하시겠습니까?",
+                    Snackbar.LENGTH_INDEFINITE
+                ).setAction(
+                    "OK"
+                ) { // 스낵바 OK 클릭시 실행작업
+                    startActivity(
+                        Intent(
+                            Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                            Uri.parse("package:" + BuildConfig.APPLICATION_ID)
+                        )
+                    )
+                }.show()
             }
         }
     }
+
+
+    /* ----------- 테스트 영역 (완성 후 삭제) ----------- */
+//    // 위치 권한이 있는지 확인하는 메서드
+//    private fun checkPermissionForLocation(context: Context): Boolean {
+//        // Android 6.0 Marshmallow 이상에서는 위치 권한에 추가 런타임 권한이 필요
+//        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//            if (context.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+//                || (context.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED)) {
+//                true
+//            } else {
+//                // 권한이 없으므로 권한 요청 알림 보내기
+//                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_PERMISSION_LOCATION)
+//                false
+//            }
+//        } else {
+//            true
+//        }
+//    }
+
+//    // 사용자에게 권한 요청 후 결과에 대한 처리 로직
+//    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+//        if (requestCode == REQUEST_PERMISSION_LOCATION) {
+//            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                // 권한 승인시, 필요하다면 Toast메세지 추가할것
+//
+//            } else {
+//                // 권한 거절시, 설정페이지로 이동하는 창 생성
+//                // #1 스낵바
+//                Snackbar.make(
+//                    introPageLayout,
+//                    "앱 사용을 위해서는 권한 설정이 필요합니다.\n설정으로 이동하시겠습니까?",
+//                    Snackbar.LENGTH_INDEFINITE
+//                ).setAction(
+//                    "OK"
+//                ) { // 스낵바 OK 클릭시 실행작업
+//                    startActivity(
+//                        Intent(
+//                            Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+//                            Uri.parse("package:" + BuildConfig.APPLICATION_ID)
+//                        )
+//                    )
+//                }.show()
+//
+////                // #2 alert dialog
+////                val builder = AlertDialog.Builder(this)
+////                builder.setMessage("앱 사용을 위해서는 권한 설정이 필요합니다.\\n설정으로 이동하시겠습니까?")
+////                builder.setPositiveButton("확인") { dialog, which ->
+////                    startActivity(
+////                        Intent(
+////                            Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+////                            Uri.parse("package:" + BuildConfig.APPLICATION_ID)
+////                        )
+////                    )
+////                }
+////                builder.setNegativeButton("취소") { dialog, which ->
+////
+////                }
+////                builder.show()
+//            }
+//        }
+//    }
+
 }
