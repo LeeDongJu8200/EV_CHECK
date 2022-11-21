45ea613e6088228d2147f7774c61acdf2e6213b1
LDJ <ldj8200@naver.com>
2022-11-21 17:57:35 +0900

    2022.11.21 LDJ #2
    1. 슬라이딩 패널 라이브러리 및 기능 구현
    2. 마커 온클릭 리스너 생성
    3. MapFragment 바인딩 뷰로 전환

--- app/src/main/java/com/example/ev_check/fragment/MapFragment.kt
+++ app/src/main/java/com/example/ev_check/fragment/MapFragment.kt
@@ -1,12 +1,7 @@
 package com.example.ev_check.fragment
 
 import android.Manifest
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
 import android.content.pm.PackageManager
-import android.graphics.Color
 import android.location.Location
 import android.os.Bundle
 import android.os.Looper
@@ -14,12 +9,13 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.RelativeLayout
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import com.example.ev_check.R
+import com.example.ev_check.databinding.FragmentMapBinding
 import com.google.android.gms.location.*
+import com.sothree.slidinguppanel.SlidingUpPanelLayout
 import net.daum.mf.map.api.MapPOIItem
 import net.daum.mf.map.api.MapPoint
 import net.daum.mf.map.api.MapView
@@ -28,16 +24,17 @@ class MapFragment : Fragment() {
 
     /* ----------- 전역변수 선언 영역 ----------- */
 
+    lateinit var binding: FragmentMapBinding // 뷰 바인딩
+    lateinit var mapView: MapView // 카카오 지도 뷰
+    val eventListener = MarkerEventListener(this)
+
     private var mFusedLocationProviderClient: FusedLocationProviderClient? = null // 현재 위치를 가져오기 위한 변수
     lateinit var mLastLocation: Location // 위치 값을 가지고 있는 객체
     internal lateinit var mLocationRequest: LocationRequest // 위치 정보 요청의 매개변수를 저장
     internal lateinit var mLocationCallback: LocationCallback
 
-    lateinit var btnDirectionTracking: View
-    lateinit var mapView:MapView
-    lateinit var mapViewContainer:RelativeLayout
-    
-    var lat: Double = 35.1467267 // 인공사 위치
+
+    var lat: Double = 35.1467267 // 인공사 위치(임시)
     var lon: Double = 126.922157
 
 
@@ -45,6 +42,86 @@ class MapFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        /* ----------- 변수 선언 및 초기화 영역 ----------- */
+        
+        // 프래그먼트의 View 객체 inflate
+        // 뷰 바인딩 사용
+        binding = FragmentMapBinding.inflate(layoutInflater)
+        val view = binding.root
+
+        // 슬라이딩 레이아웃 바인딩 및 이벤트 리스너 추가
+        val slidePanel = binding.mainFrameLayout
+        slidePanel.addPanelSlideListener(PanelEventListener())
+
+        // 맵뷰 초기화
+        mapView = MapView(requireContext())
+        mapView.setPOIItemEventListener(eventListener)
+
+        // btnDirectionTracking 토글용 boolean 변수
+        var isCheck = true
+
+        /* ----------- 지도 영역 ----------- */
+        
+        // 카카오지도 표시
+        binding.mapViewContainer.addView(mapView)
+        
+        // 실시간 위치 업데이트
+        startLocationUpdates()
+        
+        // 앱 시작과 동시에 초기위치로 카메라 이동 + 줌인
+         mapView.setMapCenterPointAndZoomLevel(MapPoint.mapPointWithGeoCoord(lat,lon),1,false)
+
+        // 방향추적 기능 없이 마커만 표시
+        mapView.currentLocationTrackingMode = MapView.CurrentLocationTrackingMode.TrackingModeOnWithMarkerHeadingWithoutMapMoving
+
+        /* ----------- 이벤트 영역 ----------- */
+
+//        // 슬라이딩 패널 관리
+//        binding.btnDirectionTracking.setOnClickListener {
+//            val state = slidePanel.panelState
+//            // 닫힌 상태일 경우 열기
+//            if (state == SlidingUpPanelLayout.PanelState.COLLAPSED) {
+//                slidePanel.panelState = SlidingUpPanelLayout.PanelState.ANCHORED
+//            }
+//            // 열린 상태일 경우 닫기
+//            else if (state == SlidingUpPanelLayout.PanelState.EXPANDED) {
+//                slidePanel.panelState = SlidingUpPanelLayout.PanelState.COLLAPSED
+//            }
+//        }
+
+
+        // btnDirectionTracking : 방향추적 버튼
+        binding.btnDirectionTracking.setOnClickListener {
+
+            if (isCheck){ // 방향 추적 기능이 꺼져 있을 때 (기본값)
+                // 방향 추적기능 켜기
+                mapView.currentLocationTrackingMode = MapView.CurrentLocationTrackingMode.TrackingModeOff
+                mapView.currentLocationTrackingMode = MapView.CurrentLocationTrackingMode.TrackingModeOnWithHeading
+                mapView.setZoomLevel(1, true) // 줌레벨 변경 이벤트
+                // binding.btnDirectionTracking.setBackgroundResource(R.drawable.ic_gps_clicked) // **버튼 이미지 변경 이벤트, 멘토링 필요
+
+            }else{
+                mapView.currentLocationTrackingMode = MapView.CurrentLocationTrackingMode.TrackingModeOff
+                mapView.currentLocationTrackingMode = MapView.CurrentLocationTrackingMode.TrackingModeOnWithMarkerHeadingWithoutMapMoving
+                mapView.setZoomLevel(1, true)
+                // binding.btnDirectionTracking.setBackgroundResource(R.drawable.ic_gps_normal)
+
+            }
+
+            // 토글 전환
+            isCheck = !isCheck
+
+
+        }
+
+        /* ----------- 기타 영역 ----------- */
+
+        mLocationRequest =  LocationRequest.create().apply {
+
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+
+        }
 
         /* ----------- 테스트 영역 (사용 후 삭제) ----------- */
 
@@ -69,89 +146,67 @@ class MapFragment : Fragment() {
 
 //        registerReceiver(receiver, intentFilter);
 
-        
-
-        /* ----------- 변수 선언 및 초기화 영역 ----------- */
-        
-        // 프래그먼트의 View 객체 inflate
-        val view =  inflater.inflate(R.layout.fragment_map, container, false) 
-
-        // 각 객체 초기화
-        btnDirectionTracking = view.findViewById(R.id.btnDirectionTracking)
-        mapView = MapView(requireContext())
-        mapViewContainer = view.findViewById(R.id.mapView)
-
-        // btnDirectionTracking 토글용 boolean 변수
-        var isCheck = true
-
-        /* ----------- 지도 영역 ----------- */
-        
-        // 카카오지도 표시
-        mapViewContainer.addView(mapView)
-        
-        // 실시간 위치 업데이트
-        startLocationUpdates()
-        
-        // 앱 시작과 동시에 초기위치로 카메라 이동 + 줌인
-        mapView.setMapCenterPointAndZoomLevel(MapPoint.mapPointWithGeoCoord(lat,lon),1,false)
-
-        // 방향추적 기능 없이 마커만 표시
-        mapView.currentLocationTrackingMode = MapView.CurrentLocationTrackingMode.TrackingModeOnWithMarkerHeadingWithoutMapMoving
-
-
-        /* ----------- 이벤트 영역 ----------- */
-        
-        // btnDirectionTracking 버튼 - 방향 추적기능 On/Off
-        btnDirectionTracking.setOnClickListener {
-
-
-            if (isCheck){ // 방향 추적 기능이 꺼져 있을 때 (기본값)
-                // 방향 추적기능 켜기
-                mapView.currentLocationTrackingMode = MapView.CurrentLocationTrackingMode.TrackingModeOff
-                mapView.currentLocationTrackingMode = MapView.CurrentLocationTrackingMode.TrackingModeOnWithHeading
-                mapView.setZoomLevel(1, true) // 줌레벨 변경 이벤트
-                btnDirectionTracking.setBackgroundResource(R.drawable.ic_gps_clicked) // **버튼 이미지 변경 이벤트, 멘토링 필요
-
-            }else{
-                mapView.currentLocationTrackingMode = MapView.CurrentLocationTrackingMode.TrackingModeOff
-                mapView.currentLocationTrackingMode = MapView.CurrentLocationTrackingMode.TrackingModeOnWithMarkerHeadingWithoutMapMoving
-                mapView.setZoomLevel(1, true)
-                btnDirectionTracking.setBackgroundResource(R.drawable.ic_gps_normal)
-
-            }
-
-            // 토글 전환
-            isCheck = !isCheck
-
-
-            // 기능 테스트용 임시 코드 - 마커 세팅
-            val marker = MapPOIItem()
-            marker.itemName = "Default Marker"
+        // 기능 테스트용 임시 코드 - 마커 세팅
+        val marker = MapPOIItem()
+        marker.apply {
+            itemName = "Default Marker"
             marker.tag = 0
             marker.mapPoint = MapPoint.mapPointWithGeoCoord(35.1538797, 126.9047647)
             marker.markerType = MapPOIItem.MarkerType.BluePin // 기본으로 제공하는 BluePin 마커 모양.
-
             marker.selectedMarkerType =
                 MapPOIItem.MarkerType.RedPin // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
-
-
-            mapView.addPOIItem(marker)
-
-
         }
+        mapView.addPOIItem(marker)
 
-        /* ----------- 기타 영역 ----------- */
 
-        mLocationRequest =  LocationRequest.create().apply {
+        /* ----------- View 리턴 ----------- */
 
-            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-
-        }
-        
         return view
     }
 
-    /* ----------- 함수, 메서드, 기타기능 영역 ----------- */
+
+    /* ----------- 함수, 메서드, 리스너, 기타기능 영역 ----------- */
+
+
+    // 슬라이딩 패널 이벤트 리스너
+    inner class PanelEventListener : SlidingUpPanelLayout.PanelSlideListener {
+        // 패널이 슬라이드 중일 때
+        override fun onPanelSlide(panel: View?, slideOffset: Float) {
+
+        }
+
+        // 패널의 상태가 변했을 때
+        override fun onPanelStateChanged(panel: View?, previousState: SlidingUpPanelLayout.PanelState?, newState: SlidingUpPanelLayout.PanelState?) {
+            if (newState == SlidingUpPanelLayout.PanelState.COLLAPSED) {
+
+            } else if (newState == SlidingUpPanelLayout.PanelState.EXPANDED) {
+
+            }
+        }
+    }
+
+
+    // 마커 클릭 이벤트 리스너
+    class MarkerEventListener(val context: MapFragment): MapView.POIItemEventListener {
+        override fun onPOIItemSelected(mapView: MapView?, poiItem: MapPOIItem?) {
+            // 마커 클릭시
+            Log.d("클릭이벤트 확인", "클릭")
+        }
+
+        override fun onCalloutBalloonOfPOIItemTouched(mapView: MapView?, poiItem: MapPOIItem?) {
+            // 말풍선 클릭 시 (Deprecated)
+            // 이 함수도 작동하지만 그냥 아래 있는 함수에 작성하자
+        }
+
+        override fun onCalloutBalloonOfPOIItemTouched(mapView: MapView?, poiItem: MapPOIItem?, buttonType: MapPOIItem.CalloutBalloonButtonType?) {
+            // 말풍선 클릭 시
+        }
+
+        override fun onDraggablePOIItemMoved(mapView: MapView?, poiItem: MapPOIItem?, mapPoint: MapPoint?) {
+            // 마커의 속성 중 isDraggable = true 일 때 마커를 이동시켰을 경우
+        }
+    }
+
 
     // 실시간 위치 갱신 함수
     private fun startLocationUpdates() {
@@ -196,10 +251,11 @@ class MapFragment : Fragment() {
         }
         mFusedLocationProviderClient!!.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper())
     }
-    override fun onPause() {
+
+    // onPause : 다른 Activity 활성화시 호출
+   override fun onPause() {
         super.onPause()
         mFusedLocationProviderClient?.removeLocationUpdates(mLocationCallback)
     }
-
 
 }
